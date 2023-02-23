a7f012b58dcf5804babe31413975d0ad3bf4af85
Carlos Tiago <ontiago@gmail.com>
2023-02-23 15:38:18 -0300

    add message to log

--- one_video_only_audio.py
+++ one_video_only_audio.py
@@ -4,4 +4,6 @@ youtube_link = sys.argv[1]
 directory = sys.argv[2]
 print("Iniciando...")
 YouTube(youtube_link).streams.filter(only_audio=True)[0].download(directory)
+name = YouTube(youtube_link).streams[0].title
+print("baixando: " + name)
 print("Finalizado!")
--- playlist.py
+++ playlist.py
@@ -12,6 +12,7 @@ for url in playlist:
     name = video.streams[0].title
     count_name = "0" + str(n) if n < 10 else str(n)
     new_name = count_name + " - " + name
+    print("baixando: " + new_name)
     stream.download(directory, new_name)
     n += 1
 print("Finalizado!")
